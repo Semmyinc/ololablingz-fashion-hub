c455daefb48b32c74d26755ba8848598ce3aa81b
Test User <stephen.esuola@gmail.com>
2026-02-12 11:02:14 +0100

    add requirements.txt file

--- ololablingz/settings.py
+++ ololablingz/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-from environs import Env
+from environ import Env
 
 env = Env()
 env.read_env()
