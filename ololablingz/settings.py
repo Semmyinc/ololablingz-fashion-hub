4276682fdb35d7aabacabdd835cda8bc794de430
Semmyinc <112482827+Semmyinc@users.noreply.github.com>
2026-02-25 11:57:07 +0100

    Update settings.py

--- ololablingz/settings.py
+++ ololablingz/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = fALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['ololablingz-fashion-hub-4.onrender.com']
 
