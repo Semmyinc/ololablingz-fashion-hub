3dedb83c396152f0887bd69118e4fc318e377a5c
Test User <stephen.esuola@gmail.com>
2026-02-25 10:41:09 +0100

    additional files

--- ololablingz/settings.py
+++ ololablingz/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -137,6 +138,14 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #     'ololablingz/static',
 # ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STORAGES = {
+#     'staticfiles':{
+#         'BACKEND': 'whitenoise.storage.ComppressedManifestStaticFilesStorage',
+#     },
+# }
+
+STATICFILES_STORAGE = 'whitenoise.storage.ComppressedManifestStaticFilesStorage'
 # media files set up 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
